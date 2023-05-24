3a091d9d367f3602113ff8aa661a6cc0044ad586
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2023-05-23 23:55:51 -0500

    display high scores

--- scripts/highscore.js
+++ scripts/highscore.js
@@ -7,3 +7,17 @@ function clearHighScores(event) {
   localStorage.removeItem("playerScores");
   window.location.href = "/"
 }
+
+showHighScores()
+function showHighScores() {
+  const playerScores = JSON.parse(localStorage.getItem("playerScores"))
+  console.log(playerScores)
+  if (!playerScores) return;
+
+  let highScoresList = document.getElementById('high-scores-list');
+  for (index = 0; index < playerScores.length; index++) {
+    let score = document.createElement("li");
+    score.innerHTML = `${index + 1}. ${playerScores[index].initials} &mdash; ${playerScores[index].score}`
+    highScoresList.appendChild(score);
+  }
+}
