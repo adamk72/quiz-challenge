2c21f776d04e60f698140fd97d25ab493524bb9a
Adam Kecskes <62404383+adamk72@users.noreply.github.com>
2023-05-23 19:33:32 -0500

    get questions to clear out when at end

--- scripts/game.js
+++ scripts/game.js
@@ -2,11 +2,14 @@ var startButton = document.getElementById("start-button");
 var timerSpan = document.getElementById("time-left");
 var timeLeft = 75
 var questionsContainer = document.getElementById("questions-container")
+var currentQuestionIndex = 0
+var currentAnswer = ""
+var answerResultElement = document.getElementById("answer-result")
 
-// there's a question, an array of choices, and an answer that index into the the array of choices.
+// there's a question, an array of choices, and an answer that we'll store globally to check against (there are better ways to do this).
 var quizItems = [
-    { "question": "Commonly used data types do NOT include:", "choices": ["strings", "booleans", "alerts", "numbers"], "answer": 2 },
-    { "question": "The flight of unladen swallow is:", "choices": ["0 mph", "1_000_000 mph", "Swallows can't fly", "45 mph, maybe"], "answer": 3 }
+    { "question": "Commonly used data types do NOT include:", "choices": ["strings", "booleans", "alerts", "numbers"], "answer": "alerts" },
+    { "question": "The flight of unladen swallow is:", "choices": ["0 mph", "1_000_000 mph", "Swallows can't fly", "45 mph, maybe"], "answer": "45 mph, maybe" }
 ]
 // get the question buttons so you can replace them with the question text
 var quizChoiceButtons = document.querySelectorAll(".choice")
@@ -14,7 +17,7 @@ var quizChoiceButtons = document.querySelectorAll(".choice")
 function startGame() {
     // get the number of quizItems in total
     const totalNumOfQuizItems = quizItems.length
-    console.log({ numOfQuestions: totalNumOfQuizItems })
+    renderQuestions(currentQuestionIndex)
     var gameInterval = setInterval(() => {
         timerSpan.textContent = timeLeft--;
         if (!timeLeft) {
@@ -23,14 +26,30 @@ function startGame() {
         }
     }, 1000);
 }
-renderQuestions(0)
-function renderQuestions(questionIndex, totalChoices) {
+function checkAnswer(event) {
+    answerResultElement.innerHTML = "Wrong!"
+    if (event.target.innerHTML === currentAnswer) {
+        answerResultElement.innerHTML = "Correct!"
+    }
+    answerResultElement.style.setProperty("display", "inline")
+
+    // length is a counting number, so have to subtract 1.
+    if (currentQuestionIndex < quizItems.length - 1) {
+        currentQuestionIndex++
+        renderQuestions(currentQuestionIndex)
+    } else {
+        questionsContainer.style.setProperty("display", "none") 
+    }
+    console.log(currentQuestionIndex, quizItems.length)
+}
+function renderQuestions(questionIndex) {
     const numOfChoices = quizItems[questionIndex].choices.length
     for (let index = 0; index < numOfChoices; index++) {
         quizChoiceButtons[index].innerHTML = quizItems[questionIndex].choices[index]
+        quizChoiceButtons[index].addEventListener("click", checkAnswer)
     }
     questionsContainer.style.setProperty("display", "inline-block")
-
+    currentAnswer = quizItems[questionIndex].answer
 }
 
 if (startButton)
